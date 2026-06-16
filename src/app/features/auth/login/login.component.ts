2b1277cda97e551ba1ab6ec0c13ae386c94d041d
Eliel181 <baleroeliel@gmail.com>
2026-06-16 11:42:45 -0300

    new: funcion para loguearse con google

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
+  isSubmitting = false;
+
+  loginForm: FormGroup;
+  errorMessage = null;
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  async onSubmit(): Promise<void> {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isSubmitting = true;
+    this.errorMessage = null;
+
+    try {
+
+      await this.authService.login(this.loginForm.value)
+
+    } catch (error: any) {
+      this.errorMessage = error.message;
+    } finally {
+      this.isSubmitting = false;
+    }
+  }
+
+  async loginWithGoogle(): Promise<void> {
+    try {
+
+      await this.authService.loginWithGoogle();
+    } catch (error) {
+      alert('Hubo un error al momento de inciar secion con google')
+    }
+  }
 }
