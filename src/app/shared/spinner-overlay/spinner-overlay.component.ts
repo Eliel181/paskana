9f1c304138c67fb43697727c0460eb36c1b46194
Eliel181 <baleroeliel@gmail.com>
2026-06-18 16:13:21 -0300

    new(component): nuevo componente spinner-overlay

---
+++ src/app/shared/spinner-overlay/spinner-overlay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner-overlay',
+  imports: [],
+  templateUrl: './spinner-overlay.component.html',
+  styleUrl: './spinner-overlay.component.css'
+})
+export class SpinnerOverlayComponent {
+
+}
